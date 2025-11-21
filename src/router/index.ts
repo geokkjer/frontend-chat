7d3c00cb9bb93c7c5d037639bf7d205b82eba78a
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-11-21 10:11:55 +0100

    prøver hash routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NavbarComponent from '@/components/NavbarComponent.vue'
 import HeroComponent from '@/components/HeroComponent.vue'
 import ChatView from '@/views/ChatView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
